f5be8536ffbe523116b64fc40b85d6e94128db89
Janos Cserep <cserepj@pilar.hu>
2024-02-20 12:17:10 +0100

    added factory method

--- src/main/java/assets/Assets.java
+++ src/main/java/assets/Assets.java
@@ -2,45 +2,50 @@ package assets;
 
 public final class Assets {
 
-    public static final String MOMENT_JS = "libs/moment-with-locales.min.js";
-    public static final String JSCROLL_JS = "libs/jquery.jscroll.js";
-    public static final String CROPPER_JS = "libs/cropper.min.js";
-    public static final String CROPPER_CSS = "libs/cropper.min.css";
-    public static final String OWL_JS = "libs/owl.carousel.js";
-    public static final String OWL_CSS = "libs/assets/owl.carousel.min.css";
-    public static final String OWL_THEME = "libs/assets/owl.theme.default.min.css";
-    private static final String BOOTSTRAP_BASE = "libs/bootstrap_latest/";
-    public static final String BOOTSTRAP_JS = BOOTSTRAP_BASE + "js/bootstrap.bundle.min.js";
-    public static final String BOOTSTRAP_CSS = BOOTSTRAP_BASE + "css/bootstrap.min.css";
-    public static final String BOOTSTRAP_SELECT2_CSS = "libs/select2-bootstrap-5-theme.min.css";
-    public static final String BOOTSTRAP_DATETIMEPICKER_JS = "libs/js/bootstrap-datetimepicker.min.js";
-    public static final String BOOTSTRAP_DATETIMEPICKER_CSS = "libs/css/bootstrap-datetimepicker.min.css";
-    public static final String FA_CSS = "libs/css/all.min.css";
-    public static final String TINYMCE = "libs/tinymce/tinymce.min.js";
-    public static final String CM_BASE = "libs/codemirror/";
-    public static final String CM_JS = CM_BASE + "lib/codemirror.js";
-    public static final String CM_CSS = CM_BASE + "lib/codemirror.css";
-    public static final String CM_MODES = CM_BASE + "mode/";
-    public static final String CM_MODE_XML = CM_MODES + "xml/xml.js";
-    public static final String CM_MODE_CSS = CM_MODES + "css/css.js";
-    public static final String LIGTHBOX_JS = "libs/bs5-lightbox/index.bundle.min.js";
-    public static final String MINDMAPMASTER_JS = "js-mindmap-master/js-mindmap.js";
-    public static final String MINDMAPMASTER_CSS = "js-mindmap-master/js-mindmap.css";
-    public static final String HTML2CANVAS_JS = "libs/html2canvas.min.js";
-    public static final String TAGGD_JS = "libs/taggd.min.js";
-    public static final String TAGGD_CSS = "libs/taggd.css";
-    public static final String SLIDER_JS = "libs/slider/js/ion.rangeSlider.min.js";
-    public static final String SLIDER_CSS = "libs/slider/css/ion.rangeSlider.css";
-    public static final String SLIDER_SKIN_CSS = "libs/slider/css/ion.rangeSlider.skinHTML5.css";
+  public static ResourceReference kamraJsAsset(final String path) {
+    return new PackageResourceReference(Assets.class, path);
+  }
 
-    public static final String TINYMCE_EMOJI = "libs/tinymce-emoji/plugin.min.js";
+  public static final String MOMENT_JS = "libs/moment-with-locales.min.js";
+  public static final String JSCROLL_JS = "libs/jquery.jscroll.js";
+  public static final String CROPPER_JS = "libs/cropper.min.js";
+  public static final String CROPPER_CSS = "libs/cropper.min.css";
+  public static final String OWL_JS = "libs/owl.carousel.js";
+  public static final String OWL_CSS = "libs/assets/owl.carousel.min.css";
+  public static final String OWL_THEME = "libs/assets/owl.theme.default.min.css";
+  private static final String BOOTSTRAP_BASE = "libs/bootstrap_latest/";
+  public static final String BOOTSTRAP_JS = BOOTSTRAP_BASE + "js/bootstrap.bundle.min.js";
+  public static final String BOOTSTRAP_CSS = BOOTSTRAP_BASE + "css/bootstrap.min.css";
+  public static final String BOOTSTRAP_SELECT2_CSS = "libs/select2-bootstrap-5-theme.min.css";
+  public static final String BOOTSTRAP_DATETIMEPICKER_JS =
+      "libs/js/bootstrap-datetimepicker.min.js";
+  public static final String BOOTSTRAP_DATETIMEPICKER_CSS =
+      "libs/css/bootstrap-datetimepicker.min.css";
+  public static final String FA_CSS = "libs/css/all.min.css";
+  public static final String TINYMCE = "libs/tinymce/tinymce.min.js";
+  public static final String CM_BASE = "libs/codemirror/";
+  public static final String CM_JS = CM_BASE + "lib/codemirror.js";
+  public static final String CM_CSS = CM_BASE + "lib/codemirror.css";
+  public static final String CM_MODES = CM_BASE + "mode/";
+  public static final String CM_MODE_XML = CM_MODES + "xml/xml.js";
+  public static final String CM_MODE_CSS = CM_MODES + "css/css.js";
+  public static final String LIGTHBOX_JS = "libs/bs5-lightbox/index.bundle.min.js";
+  public static final String MINDMAPMASTER_JS = "js-mindmap-master/js-mindmap.js";
+  public static final String MINDMAPMASTER_CSS = "js-mindmap-master/js-mindmap.css";
+  public static final String HTML2CANVAS_JS = "libs/html2canvas.min.js";
+  public static final String TAGGD_JS = "libs/taggd.min.js";
+  public static final String TAGGD_CSS = "libs/taggd.css";
+  public static final String SLIDER_JS = "libs/slider/js/ion.rangeSlider.min.js";
+  public static final String SLIDER_CSS = "libs/slider/css/ion.rangeSlider.css";
+  public static final String SLIDER_SKIN_CSS = "libs/slider/css/ion.rangeSlider.skinHTML5.css";
 
-    public static final String SELECT2_CSS = "libs/select2/css/select2.min.css";
-    public static final String SELECT2_JS = "libs/select2/js/select2.full.min.js";
-    public static final String SELECT2_I18N = "libs/select2/js/i18n/%s.js";
+  public static final String TINYMCE_EMOJI = "libs/tinymce-emoji/plugin.min.js";
 
-    private Assets() {
-        // hiding public constructor
-    }
+  public static final String SELECT2_CSS = "libs/select2/css/select2.min.css";
+  public static final String SELECT2_JS = "libs/select2/js/select2.full.min.js";
+  public static final String SELECT2_I18N = "libs/select2/js/i18n/%s.js";
 
+  private Assets() {
+    // hiding public constructor
+  }
 }
