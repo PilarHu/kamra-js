5af74837d0aea27c87987fe8665219b1c9e2f3b3
Janos Cserep <cserepj@pilar.hu>
2025-09-22 19:21:58 +0200

    fixing move

--- src/main/java/assets/Assets.java
+++ src/main/java/assets/Assets.java
@@ -1,3 +1,17 @@
+/*
+ * Copyright 2009-2024 Pilar Internet Consulting Kft.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS “AS IS” AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
 package assets;
 
 public final class Assets {
@@ -30,6 +44,7 @@ public final class Assets {
   public static final String MINDMAPMASTER_CSS = "js-mindmap-master/js-mindmap.css";
   public static final String HTML2CANVAS_JS = "libs/html2canvas.min.js";
   public static final String TAGGD_JS = "libs/taggd.min.js";
+  public static final String POPPER_JS = "libs/popper-js/popper.js";
   public static final String TAGGD_CSS = "libs/taggd.css";
   public static final String SLIDER_JS = "libs/slider/js/ion.rangeSlider.min.js";
   public static final String SLIDER_CSS = "libs/slider/css/ion.rangeSlider.css";
